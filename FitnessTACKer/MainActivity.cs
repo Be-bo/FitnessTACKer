0eb6345bf7e754363546b3b6f394979e4526c803
Matthew Hylton <wehttamh11@gmail.com>
2018-11-12 15:45:47 -0700

    Menu Bar
    Added a menu bar.
    The theme will need to be changed.
    I need to link the buttons to the pages.

--- FitnessTACKer/MainActivity.cs
+++ FitnessTACKer/MainActivity.cs
@@ -1,11 +1,13 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
 
 namespace FitnessTACKer
 {
-    [Activity(Label = "FitnessTACKer", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "FitnessTACKer", MainLauncher = true, Icon = "@mipmap/icon",Theme ="@style/Theme.AppCompat")]
+    public class MainActivity : AppCompatActivity
     {
         int count = 1;
 
@@ -16,14 +18,30 @@ namespace FitnessTACKer
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            //hide supportActionBar
-            this.ActionBar.Hide();
-
             // Get our button from the layout resource,
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
             button.Click += delegate { button.Text = $"{count++} lifts!"; };
+
+            var bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
+            bottomNavigation.NavigationItemSelected += (s, e) => {
+
+                switch (e.Item.ItemId) {
+                    case Resource.Id.action_calender:
+                        Toast.MakeText(this, "Calender Clicked", ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.action_home:
+                        Toast.MakeText(this, "Home Clicked", ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.action_workout:
+                        Toast.MakeText(this, "Workout Clicked", ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.action_settings:
+                        Toast.MakeText(this, "Settings Clicked", ToastLength.Short).Show();
+                        break;
+                }
+            };
         }
     }
 }
