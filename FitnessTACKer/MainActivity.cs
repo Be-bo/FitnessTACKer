5fd65bdddf7faa3b00ef7b20e60036a5ebf12b93
Bebo <rfiker@ucalgary.ca>
2018-11-12 11:05:08 -0700

    Testing commitment to lifting

--- FitnessTACKer/MainActivity.cs
+++ FitnessTACKer/MainActivity.cs
@@ -20,7 +20,7 @@ namespace FitnessTACKer
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            button.Click += delegate { button.Text = $"{count++} lifts!"; };
         }
     }
 }
