af86070a4f4d5d03f283fd53dcf9bb7ad2352ff4
Christilyn A <christilynarjona@outlook.com>
2018-11-25 11:55:05 -0700

    added layout for exercise list item

--- FitnessTACKer/ExerciseItem.cs
+++ FitnessTACKer/ExerciseItem.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.Collections.Generic;
+
+namespace FitnessTACKer
+{
+    public class ExerciseItem
+    {
+        public string exerciseName { get; set; }
+        public bool expanded { get; set; }
+        public List<string> sets { get; set; }
+    }
+}
--- FitnessTACKer/ExerciseViewHolder.cs
+++ FitnessTACKer/ExerciseViewHolder.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using System;
+
+namespace FitnessTACKer
+{
+    public class ExerciseViewHolder : RecyclerView.ViewHolder
+    {
+        
+
+        public ExerciseViewHolder(View itemView, Action<int> listener) : base(itemView)
+        {
+            
+            itemView.Click += (sender, e) => listener(base.Position);
+        }
+    }
+}
